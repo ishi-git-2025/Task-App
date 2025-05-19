a1e017ef09c5cb097f8c761ea4a874a996c63712
Ishita <ishitasofficialmail@gmail.com>
2025-05-19 13:43:46 +0530

    initial commit

--- server/src/controllers/task/taskController.js
+++ server/src/controllers/task/taskController.js
@@ -77,5 +77,67 @@ export const getTask = asyncHandler(async (req,res) =>{
          console.log("Error in getTask: ", error.message);
    return  res.status(500).json({ message: error.message });
   }
-})
+});
 
+export const updateTask = asyncHandler(async (req,res) => {
+  try {
+    const userId = req.user._id;
+
+    const {id} = req.params;
+    const {title, description, dueDate, priority,status, completed }= req.body;
+
+    if(!id){
+      res.status(400).json({message : "please provide a task id"})
+    }
+
+    const task = await TaskModel.findById(id);
+
+     if (!task){
+     return res.status(404).json({message:"Task not found for the given id!"})
+    }
+
+    if(!task.user.equals(userId)){ //if not the currently logged-in user
+      return res.status(401).json({message:"Not authorized to update this task"})
+    }
+
+    //update the task with the new data if provided || keep the old data
+    task.title = title || task.title; 
+    task.description = description || task.description;
+    task.dueDate = dueDate || task.dueDate;
+    task.priority = priority || task.priority;
+    task.status = status || task.status;
+    task.completed = completed || task.completed;
+
+    await task.save()
+    return res.status(200).json(task);
+  } catch (error) {
+    console.log("Error in updateTask: ", error.message);
+    return  res.status(500).json({ message: error.message });
+  }
+});
+
+export const deleteTask = asyncHandler(async (req,res) => {
+  try {
+      const userId = req.user._id;
+      const {id} = req.params;
+      
+    const task = await TaskModel.findById(id); //same as TaskModel.findOne({ _id: id })
+
+    if (!task){
+     return res.status(404).json({message:"Task not found for the given id!"})
+    }
+    
+    if(!task.user.equals(userId)){ //if not the currently logged-in user
+      return res.status(401).json({message:"Not authorized to delete this task"})
+    }
+
+    await TaskModel.findByIdAndDelete(id);
+
+    return res.status(200).json({message:"Task deleted successfully!"});
+
+  } catch (error) {
+    console.log("Error in deleteTask: ", error.message);
+    return  res.status(500).json({ message: error.message });
+  }
+  
+});
--- server/src/routes/tasksRoutes.js
+++ server/src/routes/tasksRoutes.js
@@ -1,11 +1,13 @@
 import express, {Router} from "express";
-import { createTask, getTasks, getTask } from "../controllers/task/taskController.js";
+import { createTask, getTasks, getTask, updateTask, deleteTask } from "../controllers/task/taskController.js";
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post("/task/create", protect ,createTask)
 router.get("/tasks", protect ,getTasks)
-router.get("/task/:id", protect, getTask)
+router.get("/task/:id", protect, getTask);
+router.patch("/task/:id", protect, updateTask);
+router.delete("/task/:id", protect, deleteTask);
 
 export default router;
